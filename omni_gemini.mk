0a8b522e76b47cf8d4afbef36421130fc3ccdcc4
Bruno Martins <bgcngm@gmail.com>
2016-06-08 11:08:11 +0100

    gemini: Include TZ data

--- omni_gemini.mk
+++ omni_gemini.mk
@@ -22,6 +22,10 @@ $(call inherit-product, build/target/product/embedded.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
+# Time Zone data for recovery
+PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gemini
 PRODUCT_NAME := omni_gemini
